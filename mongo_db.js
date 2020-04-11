eab0168573187546ab7c036194a3d1fa224b59d2
unknown <toyyibat.abdulkareem@gmail.com>
2020-04-11 15:36:28 +0100

    Task 3 completed with six images, nodeImages1-6

--- mongo_db.js
+++ mongo_db.js
@@ -1,8 +1,24 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/kareeba";
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+// const insertMovies = require('./interns');
+const findMovies = require('./findInterns');
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created by Kareeba!");
-  db.close();
-});
+const url = 'mongodb://localhost:27017';
+
+const dbName = 'kareeba';
+
+const client = new MongoClient(url, {useUnifiedTopology:true,
+                                    useNewUrlParser:true});
+
+client.connect(function(err) {
+    assert.equal(null, err);
+    console.log("Database created by Kareeba!");
+
+    const db = client.db(dbName);
+
+    // insertMovies(db, function(){
+      findMovies(db, function(){
+            client.close();
+        });
+    });
+  // });
